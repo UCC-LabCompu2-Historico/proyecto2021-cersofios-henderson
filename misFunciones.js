e161e2aa7844ede98e2c8c4e8ea9e2c29df75262
Sofia <sofiacersofios@hotmail.com>
2021-06-15 19:40:27 -0300

    Se realizaron cambios

--- misFunciones.js
+++ misFunciones.js
@@ -1,8 +1,14 @@
 /**
- * Función que permite mostrar u ocultar div según elección de radio button
+ * Función que permite mostrar u ocultar div según la elección de los radio button
  * @method mostrar_ocultar
  * @param {string} valor - El valor de los radio button (Triángulo, Cuadrado, Círculo o Rectángulo)
  * @return
+ */
+/**
+ * Función que permite calcular el área de las figuras (Triángulo, Cuadrado, Círculo o Rectángulo)
+ * @method CalculodeArea
+ * @param {string} id - El id de los div que contienen los datos de cada figura geométrica (Base y Altura, Lado o Radio)
+ * @return {number} Resultado del Área
  */
 function mostrar_ocultar(valor){
     if (valor == "Triangulo") {
@@ -27,9 +33,65 @@ function mostrar_ocultar(valor){
         document.getElementById("area_rectangulo").style.display = 'block';
     }
 }
-function AreaTriangulo(){
-    var parametro1, parametro2, resultado;
-    parametro1=Number(document.getElementById("BaseT").value);
-    parametro2=Number(document.getElementById("AlturaT").value);
-    document.getElementById("Resultado").value = (parametro1 * parametro2)/2;
+function CalculodeArea(id) {
+    if (id == area_triangulo) {
+        var parametro1, parametro2;
+        if (parametro1.includes(",")) {
+            parametro1 = parametro1.replace(",", ".");
+        }
+        if (parametro2.includes(",")) {
+            parametro2 = parametro2.replace(",", ".");
+        }
+        if (isNaN(parametro1)) {
+            alert("Se ingreso un valor invalido " + id);
+        }
+        if (isNaN(parametro2)) {
+            alert("Se ingreso un valor invalido " + id);
+        }
+        parametro1 = Number(document.getElementById("BaseT").value);
+        parametro2 = Number(document.getElementById("AlturaT").value);
+        document.getElementById("Resultado").value = (parametro1 * parametro2) / 2;
+    } else if (id == area_cuadrado){
+        var parametro1;
+        if (parametro1.includes(",")){
+            parametro1=parametro1.replace(",",".");
+        }
+        if (isNaN(parametro1)) {
+            alert("Se ingreso un valor invalido " + id);
+        }
+        parametro1=Number(document.getElementById("Lado").value);
+        document.getElementById("Resultado").value = parametro1 * parametro1;
+    } else if (id == area_circulo){
+        var parametro1;
+        if (parametro1.includes(",")){
+            parametro1=parametro1.replace(",",".");
+        }
+        if (isNaN(parametro1)) {
+            alert("Se ingreso un valor invalido " + id);
+        }
+        parametro1=Number(document.getElementById("Radio").value);
+        document.getElementById("Resultado").value = (parametro1 * parametro1) * Math.PI ;
+    } else if (id == area_rectangulo){
+        var parametro1, parametro2;
+        if (parametro1.includes(",")) {
+            parametro1 = parametro1.replace(",", ".");
+        }
+        if (parametro2.includes(",")) {
+            parametro2 = parametro2.replace(",", ".");
+        }
+        if (isNaN(parametro1)) {
+            alert("Se ingreso un valor invalido " + id);
+        }
+        if (isNaN(parametro2)) {
+            alert("Se ingreso un valor invalido " + id);
+        }
+        parametro1 = Number(document.getElementById("BaseR").value);
+        parametro2 = Number(document.getElementById("AlturaR").value);
+        document.getElementById("Resultado").value = parametro1 * parametro2;
+    }
 }
+function Calcular(){
+    document.getElementById("Resultado").innerHTML = CalculodeArea();
+}
+
+
